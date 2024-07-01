a93adeb174c0af48b79187590b77bf6251ac612b
Kate Mcguane <katemcguane@hotmail.com>
2024-07-01 13:37:10 +0000

    Build class-based models

--- sql_orm.py
+++ sql_orm.py
@@ -10,6 +10,35 @@ db = create_engine("postgresql:///chinook")
 base = declarative_base()
 
 
+# creat a class-based model fo the "Artist" table
+class Artist(base):
+    __tablename__ = "Artist"
+    ArtistId = Column(Integer, primary_key=True)
+    Name = Column(String)
+
+
+# create a class-based model for the "Album" table
+class Album(base):
+    __tablename__ = "Album"
+    AlbumId = Column(Integer, primary_key=True)
+    Title = Column(String)
+    ArtistId = Column(Integer, ForeignKey("Artist.ArtistId"))
+
+
+# creat a class-based model for the "Track" table
+class Track(base):
+    __tablename__ = "Track"
+    TrackId = Column(Integer, primary_key=True)
+    Name = Column(String)
+    AlbumId = Column(Integer, ForeignKey("Album.AlbumId"))
+    MediaTypeId = Column(Integer, primary_key=False)
+    GenreId = Column(Integer, primary_key=True)
+    Composer = Column(String)
+    Milliseconds = Column(Integer, primary_key=False)
+    Bytes = Column(Integer, primary_key=False)
+    UnitPrice = Column(Float)
+
+
 # instead of connection to the database directly, we will ask for a session
 # create a new instance of sessionmaker, then point to tour engine (the db)
 Session = sessionmaker(db)
