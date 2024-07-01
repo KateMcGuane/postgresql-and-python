e19bdbfc337e9a97d4d954b720ae80a5a2d2b4f4
Kate Mcguane <katemcguane@hotmail.com>
2024-07-01 13:23:28 +0000

    Finish setting up file

--- sql_orm.py
+++ sql_orm.py
@@ -3,3 +3,19 @@ from sqlalchemy import (
 )
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+
+
+# executing the instructions form the "chinook" database
+db = create_engine("postgresql:///chinook")
+base = declarative_base()
+
+
+# instead of connection to the database directly, we will ask for a session
+# create a new instance of sessionmaker, then point to tour engine (the db)
+Session = sessionmaker(db)
+# opens an actual session by calling the Session() subclass defined abouve
+session = Session()
+
+
+# creating the database using declarative base subclass
+base.metadata.create_all(db)
