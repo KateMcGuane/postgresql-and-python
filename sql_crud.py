1708bf0b9a7c48337c18b1318e7f34e110130c3d
Kate Mcguane <katemcguane@hotmail.com>
2024-07-02 08:19:06 +0000

    Add remaining entries for database

--- sql_crud.py
+++ sql_crud.py
@@ -32,14 +32,13 @@ session = Session()
 base.metadata.create_all(db)
 
 # creating records on our Programmer table
-# ada_lovelace = Programmer(
-#     first_name="Ada",
-#     last_name="Lovelace",
-#     gender="F",
-#     nationality="British",
-#     famous_for="First Programmer",
-# )
-
+ada_lovelace = Programmer(
+    first_name="Ada",
+    last_name="Lovelace",
+    gender="F",
+    nationality="British",
+    famous_for="First Programmer",
+)
 
 alan_turing = Programmer(
     first_name="Alan",
@@ -49,8 +48,52 @@ alan_turing = Programmer(
     famous_for="Modern Computing"
 )
 
+grace_hopper = Programmer(
+    first_name="Grace",
+    last_name="Hopper",
+    gender="F",
+    nationality="American",
+    famous_for="COBOL language"
+)
+
+margaret_hamilton = Programmer(
+    first_name="Margaret",
+    last_name="Hamilton",
+    gender="F",
+    nationality="American",
+    famous_for="Apollo 11"
+)
+
+bill_gates = Programmer(
+    first_name="Bill",
+    last_name="Gates",
+    gender="M",
+    nationality="American",
+    famous_for="Microsoft"
+)
+
+tim_berners_lee = Programmer(
+    first_name="Tim",
+    last_name="Berners-Lee",
+    gender="M",
+    nationality="British",
+    famous_for="World Wide Web"
+)
+
+kate_mcguane = Programmer(
+    first_name="Kate",
+    last_name="McGuane",
+    gender="F",
+    nationality="Irish",
+    known_for="Ergonomic Mugs"
+)
+
 # add each instance of our programmers to our session
-session.add(alan_turing)
+# session.add(grace_hopper)
+# session.add(margaret_hamilton)
+# session.add(bill_gates)
+# session.add(tim_berners_lee)
+session.add(kate_mcguane)
 
 # commit our session to the database
 session.commit()
